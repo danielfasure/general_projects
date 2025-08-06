f6dca94356e33c81967793c47ea971b61871cbb1
daniel <danielfasure@gmail.com>
2025-08-06 16:57:51 +0100

    uploading some work

--- src/showing_inherit.java
+++ src/showing_inherit.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class showing_inherit {
+    public static void main(String[] args) {
+       Ford classic_vec = new Ford();
+       classic_vec.CarMake = "ford fiestia";
+       classic_vec.honk();
+
+
+
+    }
 }
+class Car{
+  String CarMake;
+  int CarAge;
+  int intial_value;
+  int current_value;
+
+    }
+
+    class Ford extends Car{
+    void honk (){
+        System.out.println("your "+CarMake+" is honking ");
+
+    }
+    }
+
+
